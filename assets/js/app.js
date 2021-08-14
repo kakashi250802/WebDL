a9fd0497f81509467bb617319e9abd0ea396cb1c
kakashi250802 <ngoduong37na@gmail.com>
2021-08-14 10:33:56 +0700

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,29 @@
 let heading = document.querySelector('header > div');
 // change header style when scroll
 window.addEventListener('scroll', () => {
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        heading.classList.add('shrink');
-    } else {
-        heading.classList.remove('shrink');
-    }
-})
+        if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+            heading.classList.add('shrink');
+        } else {
+            heading.classList.remove('shrink');
+        }
+    })
+    // elemnet show on scroll
+let scroll = window.requestAnimationFrame || function(callback) {
+    window.setTimeout(callback, 1000 / 60)
+};
+
+
+isElInViewPort = (el) => {
+    let rect = el.getBoundingClientRect();
+
+    let distance = 200;
+
+    return rect.top <= (window.innerHeight - distance || document.documentElement.clientHeight - distance);
+}
+
+loop = () => {
+    el_to_show.forEach((el) => {
+        if (isElInViewPort(el)) el.classList.add('show')
+    });
+    scroll(loop);
+};
--- assets/js/index.js
+++ assets/js/index.js
@@ -6,6 +6,7 @@ document.querySelector('.courses-slide .button-next').addEventListener('click', 
 
 
 });
+
 // 
 document.querySelector('.courses-slide .button-prev').addEventListener('click', () => {
     setProgress();
@@ -17,6 +18,7 @@ document.querySelector('.courses-slide .button-prev').addEventListener('click', 
 
 // courses category list
 let coursesCategoryItems = document.querySelector('#category-courses-item');
+
 let coursesSlidesList = [{
         picDemo: './assets/img/Course image-1.svg',
         name: 'Everything You Need to Know About Business',
@@ -306,49 +308,64 @@ let coursesSlidesList = [{
         played: '4.5'
     },
 ]
+
 let coursesCategoryList = [
 
     {
+        index: 'to-right',
         name: 'Design',
         img: './assets/img/design-icon.svg',
         content: 'Learn Adobe Illustrator CC graphic design, logo design, and more with this in-depth, practical, easy-to-follow course!'
     }, {
+        index: 'to-top',
         name: 'Development',
         img: './assets/img/monitor.svg',
         content: 'Learn Adobe Illustrator CC graphic design, logo design, and more with this in-depth, practical, easy-to-follow course!'
     },
     {
+        index: 'to-top',
         name: 'IT & Software',
         img: './assets/img/connect item.svg',
         content: 'Learn Adobe Illustrator CC graphic design, logo design, and more with this in-depth, practical, easy-to-follow course!'
     },
     {
+        index: 'to-left',
         name: 'Business',
         img: './assets/img/bag-icon.svg',
         content: 'Learn Adobe Illustrator CC graphic design, logo design, and more with this in-depth, practical, easy-to-follow course!'
     },
     {
+        index: 'to-right',
+
         name: 'Marketing',
         img: './assets/img/volume.svg',
         content: 'Learn Adobe Illustrator CC graphic design, logo design, and more with this in-depth, practical, easy-to-follow course!'
     },
     {
+        index: 'to-top',
+
         name: 'Photography',
         img: './assets/img/camera.svg',
         content: 'Learn Adobe Illustrator CC graphic design, logo design, and more with this in-depth, practical, easy-to-follow course!'
     },
     {
+        index: 'to-top',
+
         name: 'Health & Care',
         img: './assets/img/heart.svg',
         content: 'Learn Adobe Illustrator CC graphic design, logo design, and more with this in-depth, practical, easy-to-follow course!'
     },
     {
+        index: 'to-right',
+
         name: 'Technology',
         img: './assets/img/connect-dislay.svg',
         content: 'Learn Adobe Illustrator CC graphic design, logo design, and more with this in-depth, practical, easy-to-follow course!'
     }
 ]
+
 let courseSlideItems = document.querySelector('#courses-slide-items');
+
 let coOperateLists = [
     './assets/img/envato-logo.svg',
     './assets/img/behance-logo.svg',
@@ -376,12 +393,15 @@ let coOperateLists = [
     './assets/img/udemy-logo.svg',
 
 ]
+
 let coOperateSlidesItems = document.querySelector('#co-operate-Slides');
+
+
 renderCategoryCourseList = () => {
     coursesSlidesList.forEach((item, index) => {
         let courseSlideItem = `
          <!-- courses item -->
-         <div class="swiper-slide" aria-label="${index + 1}/${coursesCategoryList.length}">
+         <div class="swiper-slide to-top show-on-scroll" aria-label="${index + 1}/${coursesCategoryList.length}">
         <a href="#"class="course-item" >
             <div class="img">
                 <img src="${item.picDemo}" alt="">
@@ -428,7 +448,7 @@ renderCategoryCourseList = () => {
     coursesCategoryList.forEach(item => {
         let listCourseCategory = `
         <a href="#" class="col-3">
-        <div class="category-item">
+        <div class="category-item ${item.index} show-on-scroll">
             <div class="img">
                 <img src="${item.img}" alt="">
             </div>
@@ -440,13 +460,15 @@ renderCategoryCourseList = () => {
             </div>
         </div>
     </a>`
+
         coursesCategoryItems.insertAdjacentHTML("beforeend", listCourseCategory)
+
     })
     coOperateLists.forEach((item) => {
         let listcoOperateSlides = `
     <!-- co-operate item -->
     <div class="swiper-slide">
-    <a href="#" class="co-operate-item">
+    <a href="#" class="co-operate-item ">
     <div class="img">
             <img src="${item}" alt="">
             </div>
@@ -460,14 +482,20 @@ renderCategoryCourseList = () => {
 
 }
 renderCategoryCourseList();
+
 let slideItems = document.querySelectorAll('.courses-slide .course-item');
+
 setProgress = () => {
 
     let nowIndex = parseFloat(document.querySelector('.swiper-slide-active').getAttribute("aria-label")) + 2;
-
-    console.log(nowIndex)
     let percentNow = -100 + nowIndex / slideItems.length * 100 + "% ";
     progressBar.style.transform = `translateX(${percentNow})`;
 
 }
+
 setProgress();
+let el_to_show = document.querySelectorAll('.show-on-scroll');
+setTimeout(() => {
+
+    loop();
+}, 100);
